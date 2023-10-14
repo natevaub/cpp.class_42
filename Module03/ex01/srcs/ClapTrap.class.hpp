4c369bb4819c08c0d50f353cb2c92a2a6dfe42a9
Nathan Vaubien <nathan.vaubien@gmail.com>
2023-10-14 19:32:41 +0200

    Update

--- Module03/ex01/srcs/ClapTrap.class.hpp
+++ Module03/ex01/srcs/ClapTrap.class.hpp
@@ -29,7 +29,7 @@ class ClapTrap {
 		void	takeDamage( unsigned int amount );
 		void	beRepaired( unsigned int amount );
 
-	private:
+	protected:
 		std::string		_Name;
 		unsigned int	_HitPoints;
 		unsigned int	_EnergyPoints;
--- Module03/ex01/srcs/ScavTrap.cpp
+++ Module03/ex01/srcs/ScavTrap.cpp
@@ -6,13 +6,13 @@
 /*   By: nvaubien <nvaubien@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 03:49:18 by nvaubien          #+#    #+#             */
-/*   Updated: 2023/10/14 04:13:59 by nvaubien         ###   ########.fr       */
+/*   Updated: 2023/10/14 17:25:28 by nvaubien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.class.hpp"
 
-ScavTrap::ScavTrap( std::string name) : ClapTrap(name) {
+ScavTrap::ScavTrap( std::string name ) : ClapTrap(name) {
 	this->_HitPoints = 100;
 	this->_EnergyPoints = 50;
 	this->_AttackDamage = 20;
